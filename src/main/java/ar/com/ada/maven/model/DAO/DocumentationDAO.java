73192d7c2b1f50542aed135a9bb68dfa2ff1150c
Andrea <andreacristinals9@gmail.com>
2020-01-25 02:34:55 -0300

    update DAO documentation

--- src/main/java/ar/com/ada/maven/model/DAO/DocumentationDAO.java
+++ src/main/java/ar/com/ada/maven/model/DAO/DocumentationDAO.java
@@ -1,7 +1,13 @@
 package ar.com.ada.maven.model.DAO;
 
+import ar.com.ada.maven.DBConnection;
 import ar.com.ada.maven.model.DTO.DocumentationDTO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DocumentationDAO implements DAO<DocumentationDTO> {
@@ -14,7 +20,16 @@ public class DocumentationDAO implements DAO<DocumentationDTO> {
         this.willCloseConnection = willCloseConnection;
     }
 
-    public List<DocumentationDAO> findAll(){
-
+    public List<DocumentationDTO> findAll(){
+        String sql = "SELECT * FROM documentation";
+        List<DocumentationDTO> docs = new ArrayList<>();
+        try {
+            Connection conn = DBConnection.getConnection();
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while
+        } catch (SQLException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            System.out.println("CONNECTION ERROR: " + e.getMessage());
+        }
     }
 }
