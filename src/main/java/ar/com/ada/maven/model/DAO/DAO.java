d7882c85a265210b5f2f2ce7a93dec321ecbfeac
Andrea <andreacristinals9@gmail.com>
2020-01-25 01:40:55 -0300

    create new branch DAO interface

---
+++ src/main/java/ar/com/ada/maven/model/DAO/DAO.java
@@ -0,0 +1,16 @@
+package ar.com.ada.maven.model.DAO;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+public interface DAO <T> {
+
+    Collection<T> findAll() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException;
+
+    T findBId(Integer id);
+
+    Boolean save( T t);
+    Boolean update( T t, Integer id);
+    Boolean delete (Integer id);
+    
+}
