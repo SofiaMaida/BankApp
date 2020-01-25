accd059f91b87c6cbce9441d2b097ecc209d14df
Andrea <andreacristinals9@gmail.com>
2020-01-25 00:18:46 -0300

    update DTO 2

--- src/main/java/ar/com/ada/maven/model/DTO/AccountDTO.java
+++ src/main/java/ar/com/ada/maven/model/DTO/AccountDTO.java
@@ -5,5 +5,59 @@ public class AccountDTO {
     private Integer id, number_account;
     private PersonDTO person;
 
+    public AccountDTO(){
+    }
 
+    public AccountDTO(Integer id, Integer number_account){
+        this.id = id;
+        this.number_account = number_account;
+    }
+
+    public AccountDTO(Integer id, Integer number_account, PersonDTO person){
+        this.id = id;
+        this.number_account = number_account;
+        this.person = person;
+    }
+
+    public PersonDTO getPerson() {
+        return person;
+    }
+
+    public void setPerson(PersonDTO person) {
+        this.person = person;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getNumber_account() {
+        return number_account;
+    }
+
+    public void setNumber_account(Integer number_account) {
+        this.number_account = number_account;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuenta: [-id: " +id+ " -Numero de cuenta: " +number_account+ " -Persona" +person+ "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        AccountDTO that = (AccountDTO) obj;
+        return id.equals(that.id) && number_account.equals(that.number_account) && person.equals(that.person);
+    }
+
+    @Override
+    public int hashCode() {
+        return -123 * id.hashCode() + number_account.hashCode() + person.hashCode();
+    }
 }
--- src/main/java/ar/com/ada/maven/model/DTO/Type_accountDTO.java
+++ src/main/java/ar/com/ada/maven/model/DTO/Type_accountDTO.java
@@ -1,4 +1,63 @@
 package ar.com.ada.maven.model.DTO;
 
 public class Type_accountDTO {
+
+    private Integer id;
+    private String type;
+    private AccountDTO account;
+
+    public Type_accountDTO(){}
+
+    public Type_accountDTO(Integer id, String type){
+        this.id = id;
+        this.type=type;
+    }
+
+    public Type_accountDTO(Integer id, String type, AccountDTO account){
+        this.id = id;
+        this.type=type;
+        this.account = account;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAccount(AccountDTO account) {
+        this.account = account;
+    }
+
+    public AccountDTO getAccount() {
+        return account;
+    }
+
+    @Override
+    public int hashCode() {
+        return -884 * id.hashCode() + type.hashCode() + account.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Type_accountDTO that = (Type_accountDTO) obj;
+        return id.equals(that.id) && type.equals(that.type) && account.equals(that.account);
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo de cuenta: [-id: " +id + " - Tipo de cuenta: " + type + " -cuenta: " + account + "]";
+    }
 }
