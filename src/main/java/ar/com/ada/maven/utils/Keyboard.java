01c6d08758e725ab0dbaf2248be237434a050ace
Sofia <soofi.maidana@gmail.com>
2020-02-21 20:34:17 -0300

    keyboard

--- src/main/java/ar/com/ada/maven/utils/Keyboard.java
+++ src/main/java/ar/com/ada/maven/utils/Keyboard.java
@@ -1,28 +1,69 @@
 package ar.com.ada.maven.utils;
 
+import ar.com.ada.maven.view.MainView;
+
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Keyboard {
 
     private static Scanner keyboard;
 
-    public Keyboard(){}
+    public Keyboard() {
+    }
 
-    public static Scanner getInstance(){
-        if(keyboard == null){
+    public static Scanner getInstance() {
+        if (keyboard == null) {
             keyboard = new Scanner(System.in);
         }
         return keyboard;
     }
 
-    public static void pressEnterToContinue(){
+    public static void pressEnterToContinue() {
         System.out.println("- Presiona la tecla ENTER para continuar -");
         try {
             System.in.read();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getInputString() {
+        Scanner keyboard = getInstance();
+        while (true) {
+            try {
+                System.out.print("? ");
+                String txt = keyboard.nextLine().trim();
+                while (!txt.matches("^[A-Za-záéíóúüÁÉÍÓÚÜ\\s]+$") && !txt.isEmpty()) {
+                    MainView.invalidData();
+                    txt = keyboard.nextLine();
+                }
+                return txt;
+            } catch (InputMismatchException e) {
+                MainView.invalidData();
+                keyboard.next();
+            }
+        }
+    }
+
+    public static String getInputInteger() {
+        Scanner keyboard = getInstance();
+        while (true) {
+            try {
+                System.out.print("? ");
+                String txt = keyboard.nextLine().trim();
+                while (!txt.matches("^[0-9]+$") && !txt.isEmpty()) {
+                    MainView.invalidData();
+                    txt = keyboard.nextLine();
+                }
+                return txt;
+            } catch (InputMismatchException e) {
+                MainView.invalidData();
+                keyboard.next();
+            }
+        }
+
     }
 
 }
